d1cd18ebc3d322e4d298118c0afaca0ca2335382
WADHAWANA <TUSHAR.WADHAWANA@t-systems.com>
2022-06-01 10:35:53 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-backend"
+    storage_account_name = "terraformbackendtushar"
+    container_name       = "terraform-state-file"
+    key                  = "terraformgit.tfstate"
+  }
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.99.0"
+    }
+  }
+}
+
+#Create Resource Group
+resource "azurerm_resource_group" "Tushar" {
+  name     = "Terraform-Action"
+  location = "northeurope"
+}
