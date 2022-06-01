94f3da500b64fe95cc613a32a86ba75297dc91fc
tusharvadhawana <50181155+tusharvadhawana@users.noreply.github.com>
2022-06-01 11:47:29 +0530

    new add
    new

--- main.tf
+++ main.tf
@@ -8,15 +8,18 @@ terraform {
 }
 
 terraform {
-  required_version = ">= 1.1" 
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.99.0"
+      source = "hashicorp/azurerm"
+      version = "2.99.0"
     }
   }
-  
 }
+
+provider "azurerm" {
+  features {}
+}
+
 
 
 #Create Resource Group
